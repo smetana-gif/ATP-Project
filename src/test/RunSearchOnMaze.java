ecf24747288de602e2771c748c24daa435ac4184
yuval568 <yuvaa@post.bgu.ac.il>
2026-05-02 19:28:46 +0300

    Update RunSearchOnMaze.java to 100X100

--- src/test/RunSearchOnMaze.java
+++ src/test/RunSearchOnMaze.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 public class RunSearchOnMaze {
     public static void main(String[] args) {
         IMazeGenerator mg = new MyMazeGenerator();
-        Maze maze = mg.generate(30, 30);
+        Maze maze = mg.generate(100, 100);
         SearchableMaze searchableMaze = new SearchableMaze(maze);
 
         solveProblem(searchableMaze, new BreadthFirstSearch());
